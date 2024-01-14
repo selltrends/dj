e7524a50f2a4c770cad86e47fef5dd20cfe7a63c
Alex Wang <selltrends@gmail.com>
2024-01-14 18:05:45 +0800

    add slug field

--- my_tennis_club/members/models.py
+++ my_tennis_club/members/models.py
@@ -5,6 +5,13 @@ class Member(models.Model):
     lastname = models.CharField(max_length=255)
     phone = models.IntegerField(null=True)
     joined_date = models.DateField(null=True)
+    slug = models.SlugField(default="", null=True, blank=True)
 
     def __str__(self):
         return f"{self.firstname} {self.lastname}"
+    
+    def save(self, *args, **kwargs):
+        from django.utils.text import slugify
+        slug = self.firstname + ' ' + self.lastname
+        self.slug = slugify(slug)
+        super(Member, self).save(*args, **kwargs)
